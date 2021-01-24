469528b9e212f7d4278132f71ccd2c5d9ae90cae
Kyle Yang <kyley@andrew.cmu.edu>
2021-01-24 00:45:29 -0500

    No long scientific y-axis and added figure title

--- app.py
+++ app.py
@@ -10,9 +10,15 @@ def plot():
     rin_df = pd.read_csv("dataset.csv")
     by_category = rin_df.groupby(['Fuel Category', 'Month']).sum()[['RINs','Volume (Gal.)']].reset_index()
     df = by_category.pivot(index='Month',columns='Fuel Category', values='RINs')
-    df.plot()
+    
+    fig, ax = plt.subplots()
+    fig.suptitle("RINs by Fuel Category Over 2020", fontsize = 40)
+    ax.ticklabel_format(useOffset=False, style='plain')
+
+    df.plot(ax = ax)
+
     plt.savefig('static/images/plot.png')
-    return render_template('plot.html', url ='/static/images/new_plot.png')
+    return render_template('plot.html', url ='/static/images/plot.png')
 
 if __name__ == "__main__":
     import webbrowser
