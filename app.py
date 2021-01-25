7a5be41c32670674931bf91f42b930c10f95cef9
Kyle Yang <kyley@andrew.cmu.edu>
2021-01-25 14:36:41 -0500

    Fixed caching image bug

--- app.py
+++ app.py
@@ -3,28 +3,52 @@ import pandas as pd
 from flask import Flask, render_template, Response, request
 
 app=Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/', methods=['GET','POST'])
 def plot():
-    # Formatting
+    # Formatting and setting parameters
     plt.rcParams["figure.figsize"] = (20,10)
     fig, ax = plt.subplots()
     fig.suptitle("RINs by Fuel Category Over 2020", fontsize = 40)
     ax.ticklabel_format(useOffset=False, style='plain')
-
-    # Import data and aggregate
-    rin_df = pd.read_csv("dataset.csv")
-    by_category = rin_df.groupby(['Fuel Category', 'Month']).sum()[['RINs','Volume (Gal.)']].reset_index()
-    df = by_category.pivot(index='Month',columns='Fuel Category', values='RINs')
-    
-    df.plot(ax = ax)
-    plt.savefig('static/images/plot.png')
     url = "/static/images/plot.png"
 
+    # Read form input
     output = request.form.get('options')
     checked = request.form.getlist('categories')
 
+    # Import data and aggregate
+    raw_df = pd.read_csv("dataset.csv")
+    by_category = raw_df.groupby(['Fuel Category', 'Month']).sum()[['RINs','Volume (Gal.)']].reset_index()
+    
+    # Filter based on checked fuel categories
+    filter_list = []
+    for row in by_category['Fuel Category']:
+        filter_list.append(row in checked)
+    
+    filtered = by_category[filter_list]
+    if len(filtered.index) == 0:
+        filtered = by_category
+
+    # Plot and save
+    pivoted = filtered.pivot(index='Month',columns='Fuel Category', values='RINs')
+    pivoted.plot(ax = ax)
+    plt.savefig('static/images/plot.png')
+
     return render_template('plot.html', checked = checked, output = output, url = url)
+
+# No caching at all for API endpoints.
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'no-cache, no-store, public, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    return response
 
 if __name__ == "__main__":
     import webbrowser
